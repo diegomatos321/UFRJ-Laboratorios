f171462e34be12c686a083c90b6b6a3fa42eb369
diegomatos <diegovasconcelos62@gmail.com>
2021-08-16 16:43:32 -0300

    Funcionando...

--- Estrutura de Dados/ordenacao-topologica/main.cpp
+++ Estrutura de Dados/ordenacao-topologica/main.cpp
@@ -6,89 +6,134 @@ using namespace std;
 class Node {
   private:
     int value = 0;
-    Node* next = nullptr;
+    int grauDeEntrada = 0;
+    vector<Node*> conexoes;
   
   public:
     Node(int novoValor): value(novoValor) {}
-    Node(const Node& noCopia) {
-      Node* auxNode = noCopia.getNextNode();
-
+    Node(const Node& noCopia): conexoes(noCopia.getConexoes()) {
       value = noCopia.getValue();
-
-      next = new Node(auxNode->getValue());
-      next->setNextNode(auxNode->getNextNode());
-    }
-    ~Node() {
-      delete next;
     }
 
-    void setNextNode(Node* newNext) {
-      next = newNext;
+    void addConexao(Node* novaConexao) {
+      conexoes.push_back(novaConexao);
     }
 
-    Node* getNextNode() const {
-      return next;
+    void incrementGrauDeEntrada(int value) {
+      grauDeEntrada += value;
+    }
+
+    vector<Node*> getConexoes() const {
+      return conexoes;
     }
 
     int getValue() const {
       return value;
     }
+
+    void setValue(int novoValor) {
+      value = novoValor;
+    }
+
+    int getGrauEntrada() {
+      return grauDeEntrada;
+    }
 };
 
-void inicializarGrafo(vector<Node*> grafo, unsigned int quantidadeDeNos);
-void printNode(Node* noAtual);
-void printGrafo(vector<Node*> grafo);
+void inicializarGrafo(vector<Node>& grafo, unsigned int quantidadeDeNos);
+vector<Node*> ordenacaoTopologica(vector<Node>& grafo, unsigned int quantidadeDeNos);
+void printGrafo(const vector<Node>& grafo);
+void printNode(const Node& noAtual);
 
 int main() {
   unsigned int quantidadeDeNos = 0;
-  vector<Node*> grafo;
 
   scanf("%d %d", &quantidadeDeNos);
 
+  Node arestaReferencia(0);
+  vector<Node> grafo(quantidadeDeNos, arestaReferencia);
   inicializarGrafo(grafo, quantidadeDeNos);
 
   printGrafo(grafo);
 
+  vector<Node*> ordem = ordenacaoTopologica(grafo, quantidadeDeNos);
+  for (unsigned int i = 0; i < quantidadeDeNos; i++)
+  {
+    cout << ordem[i]->getValue() << ", ";
+  }
+  cout << endl;
+
   return 0;
 }
 
-void inicializarGrafo(vector<Node*> grafo, unsigned int quantidadeDeNos) {
-  for (size_t i = 1; i <= quantidadeDeNos; i++)
+void inicializarGrafo(vector<Node>& grafo, unsigned int quantidadeDeNos) {
+  for (size_t i = 0; i < quantidadeDeNos; i++)
   {
-    Node* noAtual = new Node(i);
     int value1 = 0, value2 = 0;
+    grafo[i].setValue(i+1);
 
     scanf("%d %d", &value1, &value2);
 
     if (value1) {
-      Node* conexao1 = new Node(value1);
-      noAtual->setNextNode(conexao1);
+      // Node* conexao1 = new Node(value1);
+      grafo[value1-1].setValue(value1);
+      grafo[value1-1].incrementGrauDeEntrada(1);
+      grafo[i].addConexao(&grafo[value1-1]);
 
       if (value2) {
-        Node* conexao2 = new Node(value2);
-        conexao1->setNextNode(conexao2);
+        // Node* conexao2 = new Node(value2);
+        grafo[value2-1].incrementGrauDeEntrada(1);
+        grafo[value2-1].setValue(value2);
+        grafo[i].addConexao(&grafo[value2-1]);
       }
     }
-
-    grafo.push_back(noAtual);
   }
 }
 
-void printGrafo(vector<Node*> grafo) {
-  for (Node* noAtual : grafo) {
+vector<Node*> ordenacaoTopologica(vector<Node>& grafo, unsigned int quantidadeDeNos) {
+  vector<Node*> ordemTopologica;
+  vector<Node*> filaDeArestasComGrauDeEntradaZero;
+
+  for (unsigned int i = 0; i < quantidadeDeNos; i++)
+  {
+    if (grafo[i].getGrauEntrada() == 0) {
+      filaDeArestasComGrauDeEntradaZero.push_back(&grafo[i]);
+    }
+  }
+  
+  // int i = 0;
+  while (!filaDeArestasComGrauDeEntradaZero.empty())
+  {
+    Node* aresta_n = filaDeArestasComGrauDeEntradaZero.back();
+    filaDeArestasComGrauDeEntradaZero.pop_back();
+    ordemTopologica.push_back(aresta_n);
+
+    for (Node* conexao : aresta_n->getConexoes()) {
+      conexao->incrementGrauDeEntrada(-1);
+
+      if (conexao->getGrauEntrada() == 0) {
+        filaDeArestasComGrauDeEntradaZero.push_back(conexao);
+      }
+    }
+  }
+  
+
+  return ordemTopologica;
+}
+
+void printGrafo(const vector<Node>& grafo) {
+  for (Node noAtual : grafo) {
     printNode(noAtual);
   }
 }
 
-void printNode(Node* noAtual) {
-  cout << noAtual->getValue();
+void printNode(const Node& noAtual) {
+  cout << noAtual.getValue();
 
-  if (noAtual->getNextNode() == nullptr) {
-    cout << endl;
-    return;
-  }
-  else {
+  for (Node* aresta : noAtual.getConexoes()) {
     cout << " -> ";
-    printNode(noAtual->getNextNode());
+    cout << aresta->getValue();
   }
+
+  cout << endl;
 }
