26b6a1619d9e10ef507a9bf455257e0f02275e6f
diegomatos <diegovasconcelos62@gmail.com>
2021-09-11 23:17:43 -0300

    Maneira correta

--- Comp3/Lab-12-expressoes-lambdas-simplificadas/lambda.cpp
+++ Comp3/Lab-12-expressoes-lambdas-simplificadas/lambda.cpp
@@ -184,17 +184,17 @@ auto operator | (const Iteravel& iteravel, Functor lambda) {
     vector<decay_t<decltype(*begin(iteravel))>> tempVetor;
 
     for (auto x : iteravel) {
-      if (lambda(x)) {
+      if (invoke(lambda, x)) {
         tempVetor.push_back(x);
       }
     }
 
     return tempVetor;
   } else if constexpr(is_same_v<invoke_result_t<Functor, decltype(*begin(iteravel))>, void> == false) {
-    vector<decltype(lambda(*begin(iteravel)))> tempVetor;
+    vector<decay_t<invoke_result_t<Functor, decltype(*begin(iteravel))>>> tempVetor;
 
     for (auto x : iteravel) {
-      tempVetor.push_back(lambda(x));
+      tempVetor.push_back(invoke(lambda, x));
     }
 
     return tempVetor;
