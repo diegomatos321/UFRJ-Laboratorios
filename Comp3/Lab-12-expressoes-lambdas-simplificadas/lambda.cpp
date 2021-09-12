bbf2ec39413a62f371289bbaae3c9dac73e26534
diegomatos <diegovasconcelos62@gmail.com>
2021-09-11 22:59:09 -0300

    Solução lab 12

--- Comp3/Lab-12-expressoes-lambdas-simplificadas/lambda.cpp
+++ Comp3/Lab-12-expressoes-lambdas-simplificadas/lambda.cpp
@@ -6,15 +6,182 @@
 
 using namespace std;
 
-template<typename Tipo>
-Tipo getTipo(Tipo tipo) {
-  return tipo;
+class X {
+  public:
+    template <typename Generico>
+    Generico operator() (const Generico generico) const {
+      return generico;
+    }
+};
+
+template <typename Tipo> 
+class Cte {
+  private:
+    Tipo constante;
+
+  public:
+    Cte(Tipo valor): constante(valor) {}
+
+    template<typename Generico>
+    Tipo operator() (const Generico generico) const {
+      return constante;
+    }
+};
+
+template <typename TipoA, typename TipoB> 
+class Soma {
+  private:
+    TipoA tipoA;
+    TipoB tipoB;
+  
+  public:
+    Soma(TipoA valorA, TipoB valorB): tipoA(valorA), tipoB(valorB) {}
+
+    template<typename Generico>
+    auto operator() (const Generico generico) const {
+      return tipoA(generico) + tipoB(generico);
+    }
+};
+
+template <typename TipoA, typename TipoB> 
+class Produto {
+  private:
+    TipoA tipoA;
+    TipoB tipoB;
+  
+  public:
+    Produto(TipoA valorA, TipoB valorB): tipoA(valorA), tipoB(valorB) {}
+
+    template<typename Generico>
+    auto operator() (const Generico generico) const {
+      return tipoA(generico) * tipoB(generico);
+    }
+};
+
+template <typename TipoA, typename TipoB> 
+class Modulo {
+  private:
+    TipoA tipoA;
+    TipoB tipoB;
+  
+  public:
+    Modulo(TipoA valorA, TipoB valorB): tipoA(valorA), tipoB(valorB) {}
+
+    template<typename Generico>
+    auto operator() (const Generico generico) const {
+      return tipoA(generico) % tipoB(generico);
+    }
+};
+
+template <typename TipoA, typename TipoB> 
+class Igual {
+  private:
+    TipoA tipoA;
+    TipoB tipoB;
+  
+  public:
+    Igual(TipoA valorA, TipoB valorB): tipoA(valorA), tipoB(valorB) {}
+
+    template<typename Generico>
+    bool operator() (const Generico generico) const {
+      return tipoA(generico) == tipoB(generico);
+    }
+};
+
+template <typename Expressao> 
+class Imprime {
+  private:
+    ostream& output;
+    Expressao expressao;
+    string resto = "";
+  
+  public:
+    Imprime(ostream& out, Expressao expressao): output(out), expressao(expressao) {}
+
+    template<typename Generico>
+    void operator() (Generico generico) const {
+      output << expressao(generico) << resto;
+    }
+
+    void add(const char c) {
+      resto += c;
+    }
+};
+
+template <typename TipoA, typename TipoB>
+auto operator + (TipoA tipoA, TipoB tipoB) {
+  if constexpr(is_arithmetic_v<TipoA>)
+    return Soma<Cte<TipoA>, TipoB>(tipoA, tipoB);
+  else if constexpr(is_arithmetic_v<TipoB>)
+    return Soma<TipoA, Cte<TipoB>>(tipoA, tipoB);
+  else
+    return Soma<TipoA, TipoB>(tipoA, tipoB);
+}
+
+template <typename TipoA, typename TipoB>
+auto operator * (TipoA tipoA, TipoB tipoB) {
+  if constexpr(is_arithmetic_v<TipoA>)
+    return Produto<Cte<TipoA>, TipoB>(tipoA, tipoB);
+  else if constexpr(is_arithmetic_v<TipoB>)
+    return Produto<TipoA, Cte<TipoB>>(tipoA, tipoB);
+  else
+    return Produto<TipoA, TipoB>(tipoA, tipoB);
+}
+
+template <typename TipoA, typename TipoB>
+auto operator % (TipoA tipoA, TipoB tipoB) {
+  if constexpr(is_integral_v<TipoA>)
+    return Modulo<Cte<TipoA>, TipoB>(tipoA, tipoB);
+  else if constexpr(is_integral_v<TipoB>)
+    return Modulo<TipoA, Cte<TipoB>>(tipoA, tipoB);
+  else
+    return Modulo<TipoA, TipoB>(tipoA, tipoB);
+}
+
+template <typename TipoA, typename TipoB>
+auto operator == (TipoA tipoA, TipoB tipoB) {
+  if constexpr(is_arithmetic_v<TipoA>)
+    return Igual<Cte<TipoA>, TipoB>(tipoA, tipoB);
+  else if constexpr(is_arithmetic_v<TipoB>)
+    return Igual<TipoA, Cte<TipoB>>(tipoA, tipoB);
+  else
+    return Igual<TipoA, TipoB>(tipoA, tipoB);
+}
+
+template <typename TipoA, typename TipoB>
+auto operator << (ostream& out, Soma<TipoA, TipoB> expressao) {
+  return Imprime<Soma<TipoA, TipoB>>(out, expressao);
+}
+
+template <typename TipoA, typename TipoB>
+auto operator << (ostream& out, Produto<TipoA, TipoB> expressao) {
+  return Imprime<Produto<TipoA, TipoB>>(out, expressao);
+}
+
+template <typename TipoA, typename TipoB>
+auto operator << (ostream& out, Modulo<TipoA, TipoB> expressao) {
+  return Imprime<Modulo<TipoA, TipoB>>(out, expressao);
+}
+
+template <typename TipoA, typename TipoB>
+auto operator << (ostream& out, Igual<TipoA, TipoB> expressao) {
+  return Imprime<Igual<TipoA, TipoB>>(out, expressao);
+}
+
+auto operator << (ostream& out, X expressao) {
+  return Imprime<X>(out, expressao);
+}
+
+template <typename Tipo>
+Imprime<Tipo> operator << (Imprime<Tipo> imprime, const char& c) {
+  imprime.add(c);
+  return imprime;
 }
 
 template<typename Iteravel, typename Functor>
 auto operator | (const Iteravel& iteravel, Functor lambda) {
-  if constexpr(is_same<decltype(lambda(*begin(iteravel))), bool>::value) {
-    vector<decltype(getTipo(*begin(iteravel)))> tempVetor;
+  if constexpr(is_same_v<invoke_result_t<Functor, decltype(*begin(iteravel))>, bool> == true) {
+    vector<decay_t<decltype(*begin(iteravel))>> tempVetor;
 
     for (auto x : iteravel) {
       if (lambda(x)) {
@@ -23,8 +190,7 @@ auto operator | (const Iteravel& iteravel, Functor lambda) {
     }
 
     return tempVetor;
-  }
-  else if constexpr(is_same<decltype(lambda(*begin(iteravel))), void>::value == false) {
+  } else if constexpr(is_same_v<invoke_result_t<Functor, decltype(*begin(iteravel))>, void> == false) {
     vector<decltype(lambda(*begin(iteravel)))> tempVetor;
 
     for (auto x : iteravel) {
@@ -32,10 +198,11 @@ auto operator | (const Iteravel& iteravel, Functor lambda) {
     }
 
     return tempVetor;
-  }
-  else {
+  } else {
     for (auto x : iteravel) {
       lambda(x);
     }
   }
 }
+
+X x;
--- Comp3/Lab-12-expressoes-lambdas-simplificadas/main.cpp
+++ Comp3/Lab-12-expressoes-lambdas-simplificadas/main.cpp
@@ -1,19 +1,43 @@
 #include <iostream>
-#include <string>
-#include <initializer_list>
-#include <vector>
 
 #include "lambda.cpp"
 
 using namespace std;
 
+void exemplo();
 void teste1();
+void teste2();
+void teste3();
+void teste4();
 
 int main() {
   teste1();
+  teste2();
+  teste3();
+  teste4();
+}
+
+void exemplo() {
+  auto s = x + x + x;
+  cout << s( 8 );
 }
 
 void teste1() {
   vector<int> v1 = { 1, 2, 3, 4, 1, 1, 0, 8 };
   v1 | (x % 2 == 0) | cout << x*x + 1 << ' ';
+}
+
+void teste2() {
+  int v1[] = { 1, 2, 3, 4, 1, 1, 0, 8 };
+  v1 | (x % 2 == 0) | cout << x*x + 1 << ' ';
+}
+
+void teste3() {
+  int v1[] = { 1, 2, 3, 4, 1, 1, 0, 8 };
+  v1 | (x % 2 == 1) | x*x | cout << x << ' ';
+}
+
+void teste4() {
+  string v1[] = { "a", "b2", "cc3", "saci" };
+  v1 | []( string n ) { return n.length() % 2 == 0; } | (x+x) | cout << x << ' ';
 }
