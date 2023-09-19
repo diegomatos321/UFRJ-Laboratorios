d9c003d82de930fb588f84688ad6cf58d83dbf1c
diegomatos <diegovasconcelos62@gmail.com>
2023-09-18 23:13:11 -0300

    Cabeçalho de entrega do trabalho

--- Redes de Computadores 2023.02/Tarefa 2 - Programação Socket/HTTP WebServer/main.py
+++ Redes de Computadores 2023.02/Tarefa 2 - Programação Socket/HTTP WebServer/main.py
@@ -1,3 +1,11 @@
+'''
+    Tarefa 02 - Disciplina Redes de Computadores 2023.02
+    Professora: Carolina Gil Marcelino
+    Alunos: 
+        Diego Vasconcelos Schardosim de Matos - 120098723
+        Gabriel Trindade Corrêa               - 120019606
+'''
+
 from Core.Kernel import MyServer as HTTPWebServer
 
 server = HTTPWebServer()
