e671b38f1f14fa909993c2cabb0385bdcb19a22a
diegomatos <diegovasconcelos62@gmail.com>
2024-04-15 23:26:50 -0300

    WIP: Refatorando programa para deixa-lo mais legível

--- programaca-concorrente-2024-01/lab3/matvet-sequencial.c
+++ programaca-concorrente-2024-01/lab3/matvet-sequencial.c
@@ -5,9 +5,41 @@
 #include "timer.h"
 
 // #define TEXTO 
+#define TO_CSV
 
-FILE* ReadDimensions(const char* fileName, int *rows, int *cols)
-{
+typedef struct {
+   int rows;
+   int cols;
+   float *elements;
+} Matriz;
+
+Matriz* CreateMatrix(int rows, int cols) {
+   Matriz *result = malloc(sizeof(Matriz));
+   if (result == NULL)
+   {
+      wprintf(L"Erro ao alocar memória para matriz");
+      exit(EXIT_FAILURE);
+   }
+   
+   result->rows = rows;
+   result->cols = cols;
+   result->elements = malloc(sizeof(float) * rows * cols);
+   if (result->elements == NULL)
+   {
+      wprintf(L"Erro ao alocar memória para os dados da matriz");
+      free(result);
+      exit(EXIT_FAILURE);
+   }
+   
+   return result;
+}
+
+void FreeMatrix(Matriz *matrix) {
+   free(matrix->elements);
+   free(matrix);
+}
+
+Matriz* ReadMatrix(const char* fileName) {
    FILE *file = fopen(fileName, "rb");
    if (file == NULL)
    {
@@ -15,130 +47,118 @@ FILE* ReadDimensions(const char* fileName, int *rows, int *cols)
       exit(EXIT_FAILURE);
    }
 
-   fread(rows, sizeof(int), 1, file);
-   fread(cols, sizeof(int), 1, file);
+   int rows, cols;
+   fread(&rows, sizeof(int), 1, file);
+   fread(&cols, sizeof(int), 1, file);
 
-   return file;
+   Matriz *result = CreateMatrix(rows, cols);
+   fread(result->elements, sizeof(float), rows * cols, file);
+
+   fclose(file);
+   return result;
 }
 
-void ReadMatrix(FILE *file, float *matrix, int tam)
+Matriz *MultiplyMatrices(Matriz *A, Matriz *B)
 {
-   fread(matrix, sizeof(float), tam, file);
-}
-
-void MultiplyMatrices(float *A, float *B, int rowsA, int colsA, int colsB, float *C)
-{
-   const int rowsC = rowsA;
-   const int colsC = colsB;
-
-   for (int i = 0; i < rowsC; ++i)
+   if (A->cols != B->cols)
    {
-      for (int j = 0; j < colsC; j++)
+      wprintf(L"Não é possível multiplicar as matrizes: Erro de dimensões");
+      exit(EXIT_FAILURE);
+   }
+
+   Matriz *C = CreateMatrix(A->rows, B->cols);
+
+   for (int i = 0; i < C->rows; ++i)
+   {
+      for (int j = 0; j < C->cols; j++)
       {
-         C[i * colsC + j] = 0;
-         for (int k = 0; k < colsA; k++)
+         C->elements[i * C->cols + j] = 0.0;
+         for (int k = 0; k < A->cols; k++)
          {
-            C[i * colsC + j] += A[i * colsA + k] * B[k * colsB + j];
+            C->elements[i * C->cols + j] += A->elements[i * A->cols + k] * B->elements[k * B->cols + j];
          }
       }
    }
+
+   return C;
 }
 
-void PrintMatrix(float *matrix, int rows, int cols)
+void PrintMatrix(Matriz *matrix)
 {
-   for (int i = 0; i < rows; ++i)
+   for (int i = 0; i < matrix->rows; ++i)
    {
-      for (int j = 0; j < cols; ++j)
+      for (int j = 0; j < matrix->cols; ++j)
       {
-         printf("%f ", matrix[i * cols + j]);
+         printf("%f ", matrix->elements[i * matrix->cols + j]);
       }
       printf("\n");
    }
 }
 
-void WriteToFile(char* fileName, const int nLinhas, const int nColunas, const int TAM_VETOR, float *matriz) {
+void WriteToFile(Matriz* matrix, char *fileName) {
       FILE * arquivoSaida = fopen(fileName, "wb");
       if(arquivoSaida == NULL) {
          wprintf(L"Erro de abertura do arquivo de saída.\n");
          exit(EXIT_FAILURE);
       }
    
-      fwrite(&nLinhas, sizeof(int), 1, arquivoSaida);
-      fwrite(&nColunas, sizeof(int), 1, arquivoSaida);
-      fwrite(matriz, sizeof(float), TAM_VETOR, arquivoSaida);
+      fwrite(matrix->rows, sizeof(int), 1, arquivoSaida);
+      fwrite(matrix->cols, sizeof(int), 1, arquivoSaida);
+      fwrite(matrix->elements, sizeof(float), matrix->rows * matrix->cols, arquivoSaida);
 }
 
 int main(int argc, char *argv[])
 {
    setlocale(LC_ALL, "");
 
-   int rowsA, colsA, rowsB, colsB;
    double start, end, delta;
-   float *A, *B, *C;
 
    GET_TIME(start);
 
    if (argc < 4)
    {
-      wprintf(L"Argumentos obrigatórios: <matriz_a> <matriz_b> <arquivo saída>.\n");
+      wprintf(L"Argumentos obrigatórios: <matriz_a.bin> <matriz_b.bin> <arquivo saída.bin>.\n");
       return EXIT_FAILURE;
    }
 
-   FILE *fileA = ReadDimensions(argv[1], &rowsA, &colsA);
-   FILE *fileB = ReadDimensions(argv[2], &rowsB, &colsB);
-
-   // Verificar dimensões das matrizes
-   if (colsA != rowsB)
-   {
-      wprintf(L"Erro-- As dimensões das matrizes não são válidas.\n");
-      return EXIT_FAILURE;
-   }
-
-   const int TAM_VETOR_A = rowsA * colsA;
-   const int TAM_VETOR_B = rowsB * colsB;
-   const int TAM_VETOR_C = rowsA * colsB;
-
-   A = (float *)malloc(sizeof(float) * TAM_VETOR_A);
-   B = (float *)malloc(sizeof(float) * TAM_VETOR_B);
-   C = (float *)malloc(sizeof(float) * TAM_VETOR_C);
-   if (A == NULL || B == NULL || C == NULL)
-   {
-      wprintf(L"Erro de alocação da memória da matriz.\n");
-      return EXIT_FAILURE;
-   }
-
-   ReadMatrix(fileA, A, TAM_VETOR_A);
-   ReadMatrix(fileB, B, TAM_VETOR_B);
-
-   fclose(fileA);
-   fclose(fileB);
+   Matriz *A = ReadMatrix(argv[1]);
+   Matriz *B = ReadMatrix(argv[2]);
 
    GET_TIME(end);
    delta = end - start;
+   #if !defined(TO_CSV)
    wprintf(L"Tempo inicializacao: %lf\n", delta);
+   #endif
 
    GET_TIME(start);
 
-   MultiplyMatrices(A, B, rowsA, colsA, colsB, C);
+   Matriz *C = MultiplyMatrices(A, B);
 
    GET_TIME(end)
    delta = end - start;
+   #ifdef TO_CSV
+   printf("\"%lf\",,%dx%d\n", delta, C->rows, C->cols); // Tempo, threads em branco e dimensões
+   #else
    wprintf(L"Tempo multiplicação das matrizes: %lf\n", delta);
+   #endif
 
    #ifdef TEXTO
    PrintMatrix(C, rowsA, colsB);
    #endif
    
-   WriteToFile(argv[3], rowsA, colsB, TAM_VETOR_C, C);
+   WriteToFile(C, argv[3]);
 
    // liberacao da memoria
    GET_TIME(start);
-   free(A);
-   free(B);
-   free(C);
+   FreeMatrix(A);
+   FreeMatrix(B);
+   FreeMatrix(C);
    GET_TIME(end)
    delta = end - start;
+
+   #if !defined(TO_CSV)
    printf("Tempo finalizacao: %lf\n", delta);
+   #endif
 
    return EXIT_SUCCESS;
 }
