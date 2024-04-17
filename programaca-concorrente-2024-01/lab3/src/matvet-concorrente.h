ebb5d2efa02567aeb5ccbdee66c34e0c80015e1b
diegomatos <diegovasconcelos62@gmail.com>
2024-04-16 22:23:49 -0300

    Reestruturando hierarquia de diretorio do projeto

--- programaca-concorrente-2024-01/lab3/src/matvet-concorrente.h
+++ programaca-concorrente-2024-01/lab3/src/matvet-concorrente.h
@@ -1,7 +1,7 @@
-// #include <stdio.h>
-// #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include "commons/Matriz.h"
 #include <pthread.h>
 #include <locale.h>
-// #include <wchar.h>
+#include <wchar.h>
 #include "timer.h"
